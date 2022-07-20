40d158ce34d61102da753742eef4e2adb5c3813b
Esteban Bardolet <estebanbardolet@iMac-de-Esteban.local>
2022-07-20 12:27:50 +0200

    Contact with the Api, Bootstrap installation and start of the table

---
+++ src/components/TableCoins.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const TableCoins = ({coins}) => {
+  return (
+    <table className='table'>
+        <thead>
+            <tr>
+                <td>name</td>
+            </tr>
+        </thead>
+        <tbody>
+            {coins.map(coin => (<tr>{coin.name}</tr>))}
+        </tbody>
+    </table>
+  )
+}
+
+export default TableCoins
